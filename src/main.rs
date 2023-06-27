6f0523601b3d171f1f5886e27405d9c72eb03454
dinaraparanid <dinaraparanid@gmail.com>
2023-06-28 00:37:09 +0300

    started to generate

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
-use music_generator::midi::parser::midi_file_manager::*;
+use ghakuf::writer::Writer;
+
+use music_generator::midi::{
+    generator::{analyzer::analyze_midi, composer::*},
+    parser::midi_file_manager::*,
+};
+
+use music_generator::notes::note::Note;
+use std::path::Path;
 
 #[monoio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    extract_notes()
-        .await?
-        .into_iter()
-        .for_each(|(path, notes)| {
-            println!("------------------ File: {:?} ------------------", path);
+    let path = Path::new("./example.mid");
+    let mut writer = Writer::new();
 
+    writer.running_status(true);
+
+    let notes = extract_notes().await?;
+
+    let melody_notes = notes
+        .into_iter()
+        .map(|(_, notes)| {
             notes
                 .into_iter()
-                .for_each(|note| println!("Note: {:?}", note));
+                .filter(|&note| note.get_note() >= Note::A4)
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
 
-            println!("\n\n\n")
-        });
+    let generated_lead = generate_from_midi_analyze(analyze_midi(melody_notes), generate_note)
+        .expect("`midi` folder is empty");
 
+    generated_lead.iter().for_each(|m| writer.push(m));
+
+    writer.write(&path)?;
     Ok(())
 }
--- src/midi/generator/composer.rs
+++ src/midi/generator/composer.rs
@@ -1,17 +1,16 @@
-use crate::{
-    midi::bpm::BPM,
-    notes::{note_data::NoteData, ChordData},
-};
-
-use ghakuf::messages::{Message, MetaEvent};
-use rand::prelude::SliceRandom;
+use crate::notes::{note_data::NoteData, ChordData};
+use ghakuf::messages::Message;
+use rand::{prelude::SliceRandom, Rng};
 use std::{collections::HashMap, hash::Hash};
 
 #[inline]
-pub fn create_note(note: NoteData) -> Vec<Message> {
+pub fn generate_note(note: NoteData) -> Vec<Message> {
+    let mut rng = rand::thread_rng();
+    let delay = rng.gen_range(0..500);
+
     vec![
-        note.into_on_midi_event(note.get_start()),
-        note.into_off_midi_event(note.get_length()),
+        note.into_on_midi_event(delay),
+        note.into_off_midi_event(delay + note.get_length()),
     ]
 }
 
@@ -19,7 +18,7 @@ pub fn create_note(note: NoteData) -> Vec<Message> {
 pub fn create_chord(mut chord: ChordData) -> Vec<Message> {
     let mut result = chord
         .iter()
-        .map(|note_data| note_data.into_on_midi_event(note_data.get_start()))
+        .map(|note_data| note_data.into_on_midi_event(0))
         .collect::<Vec<_>>();
 
     chord.sort_by_key(|nd| nd.get_length());
@@ -50,25 +49,17 @@ where
     T: Eq + Hash + Clone,
     G: Fn(T) -> Vec<Message>,
 {
-    let bpm = 90; // TODO: change BPM
-    let tempo = bpm.get_tempo();
-
-    let mut write_messages = vec![Message::MetaEvent {
-        delta_time: 0,
-        event: MetaEvent::SetTempo,
-        data: [(tempo >> 16) as u8, (tempo >> 8) as u8, tempo as u8].to_vec(),
-    }];
-
     let mut first_parts = midi_data.keys().map(|nd| nd.clone()).collect::<Vec<_>>();
     let mut rng = rand::thread_rng();
     first_parts.shuffle(&mut rng);
 
     let first_part = first_parts.first()?.clone();
     let mut prev_part = first_part.clone();
-    write_messages.extend(generator(first_part));
+
+    let mut write_messages = generator(first_part);
 
     write_messages.extend(
-        (1..8)
+        (1..24)
             .map(|_| {
                 let mut second_parts = midi_data.get(&prev_part)?.iter().fold(
                     vec![],
@@ -86,12 +77,6 @@ where
             .filter_map(|part_opt| part_opt.map(|part| generator(part)))
             .flatten(),
     );
-
-    write_messages.push(Message::MetaEvent {
-        delta_time: 0,
-        event: MetaEvent::EndOfTrack,
-        data: Vec::new(),
-    });
 
     Some(write_messages)
 }
